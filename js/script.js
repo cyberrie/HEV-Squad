b654eec71b137945071c92705e1d408f3105d3ed
helena <helena.gilja@gmail.com>
2023-02-02 20:06:21 +0000

    js logic started

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let questions = [
+  {
+    title: "Are you thirsty?",
+    choices: ["Mocktails", "Cocktail", "Soft Drink", "Water", "Surprise Me!"],
+  },
+  {
+    title: "What do you fancy?",
+    choices: ["Vegan", "Vegetarian", "Meat", "Seafood", "Surpise Me!"],
+  },
+];
