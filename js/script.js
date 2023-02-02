1472b509f5a7b3d980aa0b4b15afa7bacc6365c3
helena <helena.gilja@gmail.com>
2023-02-02 22:32:02 +0000

    click event added to emojis and tested with the questionTitle only, DOM elements added into script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+//DOM elements
+let landingPage = document.querySelector(".emoji-wrap");
+let landingPageIcons = document.getElementById("landingPageIcons");
+let angry = document.querySelector("#selectAngry");
+let happy = document.querySelector("#selectHappy");
+let sad = document.querySelector("#selectSad");
+let salsa = document.querySelector("#selectSalsa");
+let emojiBtns = document.querySelectorAll(".img-fluid");
+let emojiBtn = document.querySelectorAll(".img-fluid");
+let questionTitle = document.querySelector(".questionTitle");
+
+// Questions based of which cocktails/ food will be rendered
 let questions = [
   {
     title: "Are you thirsty?",
@@ -8,6 +20,23 @@ let questions = [
     choices: ["Vegan", "Vegetarian", "Meat", "Seafood", "Surpise Me!"],
   },
 ];
-//Global variables
 
-let landingPageIcons = document.getElementById("landingPageIcons");
+emojiBtns.forEach(function (emojiBtn) {
+  // addEventListener to emojiBtn
+  emojiBtn.addEventListener("click", function (event) {
+    // hide landing page
+    landingPage.setAttribute("class", "hide");
+
+    // set the click target and store it in a variable
+    let selectedEmoji = event.target;
+
+    if (
+      selectedEmoji === angry ||
+      selectedEmoji === happy ||
+      selectedEmoji === sad ||
+      selectedEmoji === salsa
+    ) {
+      questionTitle.innerHTML = questions[0].title;
+    }
+  });
+});
