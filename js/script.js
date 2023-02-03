3f346b5fd24838ecf10892052f24c5496eca3155
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-02-03 10:09:44 +0000

    testing api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 //Global variables
 
 let landingPageIcons = document.getElementById('landingPageIcons');
+
+
+let queryURL = `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`;
+fetch(queryURL)
+    .then((response) => response.json())
+    .then((response) => {
+
+        console.log(response);
+})
