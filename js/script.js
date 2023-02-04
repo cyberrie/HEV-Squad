5f006a4149d39ce17669c99247224d0ac3cff454
helena <helena.gilja@gmail.com>
2023-02-04 14:09:52 +0000

    as per previous commit but added to all meal options

--- js/script.js
+++ js/script.js
@@ -310,15 +310,23 @@ function fetchMealData(category) {
         let mealIngr = stringIngr;
         let ingrQuant = stringMeasure;
         let ingredients = mealIngr
-          .map((ingredient, index) => `${ingredient}, ${ingrQuant[index]}; `)
-          .join("");
+        .map((ingredient, index) => `<li>${ingredient}, ${ingrQuant[index]}</li>`)
+        .join("");
 
-        let htmlMealData = `<h1>${mealName}</h1>
-          <img src='${mealImg}'>
-          <li>Ingredients: ${ingredients}</li>
-          <p>Instructions: ${mealInstr}</p>`;
+      let htmlMealData = `<h1 class="card-title">${mealName}</h1>
+      <img class="card-img-top" alt="image of a meal" src='${mealImg}'>
+      <ul>${ingredients}</ul>
+      <p class="card-text">Instructions: ${mealInstr}</p>`;
 
         mealCard.innerHTML = htmlMealData;
+        resultsContainer.classList.remove("hide")
+        resultsContainer.classList.add("results-container")
+        // Remove the Questions
+        questionTitle.innerHTML = ''
+        // Remove Buttons
+        document.querySelector(".food-btns").innerHTML = ''
+        let containerTextHtml = `<div><button>Save</button></div>`
+       container.innerHTML = containerTextHtml
       });
   } else {
     fetch(`https://themealdb.com/api/json/v1/1/filter.php?c=${category}`)
@@ -361,14 +369,23 @@ function fetchMealData(category) {
         let mealIngr = stringIngr;
         let ingrQuant = stringMeasure;
         let ingredients = mealIngr
-          .map((ingredient, index) => `${ingredient} ${ingrQuant[index]}`)
-          .join("");
-        let htmlMealData = `<h1>${mealName}</h1>
-          <img src='${mealImg}'
-          <li>Ingredients: ${ingredients}</li>
-          <p>${mealInstr}</p>`;
+        .map((ingredient, index) => `<li>${ingredient}, ${ingrQuant[index]}</li>`)
+        .join("");
+
+      let htmlMealData = `<h1 class="card-title">${mealName}</h1>
+      <img class="card-img-top" alt="image of a meal" src='${mealImg}'>
+      <ul>${ingredients}</ul>
+      <p class="card-text">Instructions: ${mealInstr}</p>`;
 
         mealCard.innerHTML = htmlMealData;
+        resultsContainer.classList.remove("hide")
+        resultsContainer.classList.add("results-container")
+        // Remove the Questions
+        questionTitle.innerHTML = ''
+        // Remove Buttons
+        document.querySelector(".food-btns").innerHTML = ''
+        let containerTextHtml = `<div><button>Save</button></div>`
+       container.innerHTML = containerTextHtml
       });
   }
 }
