9926b2e5bc70e86da069334b22b0b9da154ea45c
helena <helena.gilja@gmail.com>
2023-02-08 17:52:33 +0000

    shuffle for drinks img fixed, ls cards styled, sort of

--- js/favourites.js
+++ js/favourites.js
@@ -15,19 +15,19 @@ function displayFavourites() {
   console.log(storedCards);
 
   for (let i = 0; i < storedCards.length; i += 2) {
-    // display meal and drink pairs
+    // display 2 cards next to each other
     let meal = storedCards[i];
     let drink = storedCards[i + 1];
 
     // render them one next to each other
     let message = `<div class="results-wrap">
-    <div class='card'>
+    <div class='card' id='card-one'>
       <h1 class="card-title">${meal.cardName}</h1>
       <img class="card-img-top" id="cardImg" alt="image of a meal" src=${meal.cardImg}>
       <p style='font-size: 15px;'class="card-text">Ingredients:</p><ul>${meal.ingredients}</ul>
       <p style='font-size: 15px;'class="card-text"> Instructions: ${meal.cardInstructions}</p>
     </div>
-    <div class='card'>
+    <div class='card' id='card-two'>
       <h1 class="card-title">${drink.cardName}</h1>
       <img class="card-img-top" id="cardImg" alt="image of a drink" src=${drink.cardImg}>
       <p style='font-size: 15px;'class="card-text">Ingredients:</p><ul>${drink.ingredients}</ul>
@@ -41,6 +41,24 @@ function displayFavourites() {
 
     favourites.appendChild(li);
   }
+
+  setCardHeight();
 }
 
 displayFavourites();
+
+// Drink card height to match the meal card height
+function setCardHeight() {
+  const card1 = document.getElementById("card-one");
+  const card2 = document.getElementById("card-two");
+
+  console.log(`card1 height: ${card1.offsetHeight}`);
+
+  window.requestAnimationFrame(() => {
+    const cardHeight = card2.offsetHeight;
+
+    console.log(`Setting card2 height to: ${cardHeight}`);
+
+    card2.style.height = cardHeight;
+  });
+}
